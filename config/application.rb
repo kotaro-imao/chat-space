6fe94d69c08d2b3f5afcf4c0bdcf35e491f16505
kotaro-imao <kotaroima4@gmail.com>
2020-07-26 19:13:07 +0900

    API

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
@@ -15,5 +16,6 @@ module ChatSpace
       g.test_framework false
       config.i18n.default_locale = :ja
     end
+    
   end
 end
